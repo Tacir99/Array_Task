f0191a9f6fc545ab84157c22cdafd384f31b58eb
Tacir Kazimov <kazimovtacir2003@gmail.com>
2024-04-29 23:58:44 +0400

    Add project files.

---
+++ Task1_Arrays/Program.cs
@@ -0,0 +1,164 @@
+﻿
+Console.Write("Enter the number of elements of the array: ");
+int arraySize = int.Parse(Console.ReadLine());
+int[] numbers = new int[arraySize];
+
+var random = new Random();
+
+for (int i = 0; i < arraySize; i++)
+{
+    numbers[i] = random.Next(100);
+}
+
+foreach (var number in numbers)
+{
+    Console.Write($"{number}  ");
+}
+
+#region Ascending order
+
+for (int i = 0; i < arraySize - 1; i++)
+{
+    for (int j = i + 1; j < arraySize; j++)
+    {
+        if (numbers[i] > numbers[j])
+        {
+            int orderAsc = numbers[i];
+            numbers[i] = numbers[j];
+            numbers[j] = orderAsc;
+        }
+    }
+
+}
+Console.Write("\nAscending order:  ");
+foreach (var item in numbers)
+{
+    Console.Write(item + " ");
+}
+
+#endregion
+
+#region Descending order
+for (int i = 0; i < arraySize - 1; i++)
+{
+    for (int j = i + 1; j < arraySize; j++)
+    {
+        if (numbers[i] < numbers[j])
+        {
+            int orderDes = numbers[i];
+            numbers[i] = numbers[j];
+            numbers[j] = orderDes;
+        }
+    }
+
+}
+Console.Write("\nDescending order:  ");
+foreach (var item in numbers)
+{
+    Console.Write(item + " ");
+}
+#endregion
+
+#region Maximum Element
+void maxNum()
+{
+    int max = numbers[0];
+    for (int i = 0; i < arraySize; i++)
+    {
+        if (numbers[i] > max)
+        {
+            max = numbers[i];
+        }
+
+    }
+    Console.WriteLine($"\nThe maximum element of the array: {max}");
+}
+
+maxNum();
+
+void maxNumSecond()
+{
+    int max = numbers[0];
+    for (int i = 0; i < arraySize; i++)
+    {
+        if (numbers[i] > max)
+        {
+            max = numbers[i];
+        }
+
+    }
+    int secondMax = 0;
+    for (int i = 0; i < arraySize; i++)
+    {
+        if (numbers[i] != max && numbers[i] > secondMax)
+        {
+            secondMax = numbers[i];
+        }
+    }
+    Console.WriteLine($"\nThe second maximum element of the array: {secondMax}");
+}
+maxNumSecond();
+#endregion
+
+#region Minimum Element
+
+void minNum()
+{
+    int min = numbers[0];
+    for (int i = 0; i < arraySize; i++)
+    {
+        if (numbers[i] < min)
+        {
+            min = numbers[i];
+        }
+
+    }
+    Console.WriteLine($"\nThe minimum element of the array: {min}");
+}
+minNum();
+#endregion
+
+
+
+#region IndexOf
+
+
+int sum = 0;
+int total = 1;
+
+for (int i = 0; i < arraySize; i++)
+{
+    if (i != 0)
+    {
+        if (i % 2 == 0)
+        {
+            total = total * numbers[i];
+        }
+        else
+        {
+            sum = sum + numbers[i];
+        }
+
+    }
+}
+
+Console.WriteLine($"\nSum of elements with odd indices: {sum}");
+Console.WriteLine($"\nMultiplication of elements with even indices: {total}");
+
+#endregion
+
+
+#region Average
+void avgNumbers()
+{
+    double sum = 0;
+    for (int i = 0; i < arraySize; i++)
+    {
+        sum += numbers[i];
+    }
+    Console.WriteLine($"\nAverage number of array: {sum / arraySize}");
+}
+
+avgNumbers();
+#endregion
+
---
+++ Task2_Arrays/Program.cs
@@ -0,0 +1,77 @@
+﻿
+Console.Write("Enter the number of elements of the first array: ");
+int arraySizeOne = int.Parse(Console.ReadLine());
+int[] numbers = new int[arraySizeOne];
+
+Console.Write("Enter the number of elements of the second array: ");
+int arraySizeTwo = int.Parse(Console.ReadLine());
+int[] numbers2 = new int[arraySizeTwo];
+
+var random = new Random();
+Console.WriteLine("Array One: ");
+for (int i = 0; i < arraySizeOne; i++)
+{
+    numbers[i] = random.Next(100);
+}
+
+foreach (var number in numbers)
+{
+    Console.WriteLine($"{number}  ");
+}
+Console.WriteLine("Array Two: ");
+for (int i = 0; i < arraySizeTwo; i++)
+{
+    numbers2[i] = random.Next(100);
+}
+
+foreach (var number in numbers2)
+{
+    Console.WriteLine($"{number}  ");
+}
+
+int newSize = 0;
+foreach (int element in numbers)
+{
+    if (element % 2 == 0)
+    {
+        newSize++;
+    }
+}
+foreach (int element in numbers2)
+{
+    if (element % 2 != 0)
+    {
+        newSize++;
+    }
+}
+
+int[] newArray = new int[newSize]; 
+
+int index = 0;
+
+
+foreach (int element in numbers)
+{
+    if (element % 2 == 0)
+    {
+        newArray[index] = element;
+        index++;
+    }
+}
+
+
+foreach (int element in numbers2)
+{
+    if (element % 2 != 0)
+    {
+        newArray[index] = element;
+        index++;
+    }
+}
+
+
+Console.WriteLine("New Array: ");
+foreach (int element in newArray)
+{
+    Console.Write(element + " ");
+}
